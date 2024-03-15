e3ff209ff9815d39bbe8282aeba318000c56b67e
xyugen <alex.arias.jorym@gmail.com>
2024-03-15 18:15:45 +0800

    feat: add diced list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Toaster, toast } from 'sonner';
 import './App.css'
 import DiceButton from './Components/DiceButton'
+import { useState } from 'react';
 
 const App = () => {
-  const diceNumbers = Array.from({ length: 12 }, (_, index) => index + 1);
+  const diceNumbers: number[] = Array.from({ length: 12 }, (_, index) => index + 1);
+  const [diceRolls, setDiceRolls] = useState([]);
   
   const handleReroll = () => {
     toast.info('Re-rolled', {
@@ -31,8 +33,14 @@ const App = () => {
         ))}
       </div>
 
-      <div className='list'>
-
+      <div className='diced-list'>
+        {diceRolls.map((diceRoll, index) => (
+          <div className='diced-number' key={index}>
+            <input name={'dice' + index} id={'dice' + index} type="checkbox" />
+            <label htmlFor={'dice' + index}>{diceRoll}</label>
+          </div>
+        ))
+        }
       </div>
       <Toaster toastOptions={{
         unstyled: true,
